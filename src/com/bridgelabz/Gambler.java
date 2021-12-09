d4637c13533aa8bf29feb1325d16e3da5ef9608e
amolnagose12 <amolnagose12@gmail.com>
2021-12-10 01:55:58 +0530

    Profit or loss after 20 Days

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -2,31 +2,50 @@ package com.bridgelabz;
 
 public class Gambler {
     /*
-    store initial capital and risk per game
+     * Creating a gamblerSimulator
+     * Created a method startGambling to store initial capital and risk per game
+     * Created a method playFor20Days which calculates amount after 20 days
      * */
 
-
-    public void startGambling() {
-        int startingAmount=100;
+    public int startGambling() {
+        int startingAmount = 100;
         int bet_Per_Game = 1;
-        while ( startingAmount != 150 || startingAmount !=50) {
+        while (startingAmount != 150 || startingAmount != 50) {
             int bet_result = (int) (Math.random() * 10) % 2;
             if (bet_result == 0) {
                 startingAmount -= bet_Per_Game;
             } else {
                 startingAmount += bet_Per_Game;
             }
-            if(startingAmount == 150) {
-                System.out.println("Won 50% of the stake ");
-                break;}
-            else if (startingAmount == 50)
-            {
-                System.out.println("Lost 50 % of the stake");
-                break;}
+            if (startingAmount == 150) {
+                break;
+            } else if (startingAmount == 50) {
+                break;
+            }
         }
+        return startingAmount;
     }
-    public static void main (String[]args){
+
+    public void playFor20Days() {
+        int total_Amount_Invested_In_20_Days = 2000;
+        int amount_After_20Days =0;
+        for (int i = 1; i <= 20; i++) {
+            amount_After_20Days += startGambling();
+        }
+        System.out.println("Amount after 20 day is "+ amount_After_20Days);
+        if (amount_After_20Days > total_Amount_Invested_In_20_Days) {
+            int profit = amount_After_20Days - total_Amount_Invested_In_20_Days;
+            System.out.println("Total profit after 20 days is = " + profit);
+
+        } else if (amount_After_20Days < total_Amount_Invested_In_20_Days){
+            int loss = total_Amount_Invested_In_20_Days - amount_After_20Days;
+            System.out.println("Total loss after 20 days is = " + loss);
+        }
+        else System.out.println("No profit No loss");
+    }
+
+    public static void main(String[] args) {
         Gambler play = new Gambler();
-        play.startGambling();
+        play.playFor20Days();
     }
 }
