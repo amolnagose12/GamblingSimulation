ed17b8acfdd487389027333e18907bfbceaf0140
amolnagose12 <amolnagose12@gmail.com>
2021-12-10 01:45:43 +0530

    Gambler make  bet so either win or loose

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -8,5 +8,17 @@ public class Gambler {
     public void startGambling() {
         int startingAmount = 100;
         int bet_Per_Game = 1;
+
+        int bet_result = (int) (Math.random() * 10) % 2;
+        if (bet_result == 0){
+            startingAmount-=1;
+        }
+        else startingAmount+=1;
+
+    }
+
+    public static void main(String[] args) {
+        Gambler play = new Gambler();
+        play.startGambling();
     }
 }
