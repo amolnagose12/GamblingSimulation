05faff9a01378a6bd9b040615a5ad1c09b3eaa49
amolnagose12 <amolnagose12@gmail.com>
2021-12-10 02:12:41 +0530

    If Won last month play again

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -29,7 +29,7 @@ public class Gambler {
     public void playFor20Days() {
         int total_Amount_Invested_In_20_Days = 2000;
         int amount_After_20Days = 0;
-
+        int profit=0;
         for (int i = 1; i <= 20; i++) {
             int result = startGambling();
             amount_After_20Days += result;
@@ -42,13 +42,19 @@ public class Gambler {
         }
         System.out.println("Amount after 20 day is " + amount_After_20Days);
         if (amount_After_20Days > total_Amount_Invested_In_20_Days) {
-            int profit = amount_After_20Days - total_Amount_Invested_In_20_Days;
+            profit = amount_After_20Days - total_Amount_Invested_In_20_Days;
             System.out.println("Total profit after 20 days is = " + profit);
 
         } else if (amount_After_20Days < total_Amount_Invested_In_20_Days) {
             int loss = total_Amount_Invested_In_20_Days - amount_After_20Days;
             System.out.println("Total loss after 20 days is = " + loss);
         } else System.out.println("No profit No loss");
+        if (amount_After_20Days > total_Amount_Invested_In_20_Days){
+            System.out.println("===============================================");
+            System.out.println("Won "+profit+"$ Last month. Play Again");
+            playFor20Days();
+        }
+
     }
 
     public static void main(String[] args) {
