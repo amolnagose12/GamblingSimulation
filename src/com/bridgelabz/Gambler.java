20376e1305ea54680d1dc89db008de93224fe689
amolnagose12 <amolnagose12@gmail.com>
2021-12-10 01:49:47 +0530

    game will end if 50% of stake win or loose

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -2,22 +2,30 @@ package com.bridgelabz;
 
 public class Gambler {
     /*
-     * Creating a gamblerSimulator
-     * Created a method startGambling to store initial capital and risk per game
+    store initial capital and risk per game
      * */
+
+
     public void startGambling() {
-        int startingAmount = 100;
+        int startingAmount=100;
         int bet_Per_Game = 1;
-
-        int bet_result = (int) (Math.random() * 10) % 2;
-        if (bet_result == 0){
-            startingAmount-=1;
+        while ( startingAmount != 150 || startingAmount !=50) {
+            int bet_result = (int) (Math.random() * 10) % 2;
+            if (bet_result == 0) {
+                startingAmount -= bet_Per_Game;
+            } else {
+                startingAmount += bet_Per_Game;
+            }
+            if(startingAmount == 150) {
+                System.out.println("Won 50% of the stake ");
+                break;}
+            else if (startingAmount == 50)
+            {
+                System.out.println("Lost 50 % of the stake");
+                break;}
         }
-        else startingAmount+=1;
-
     }
-
-    public static void main(String[] args) {
+    public static void main (String[]args){
         Gambler play = new Gambler();
         play.startGambling();
     }
