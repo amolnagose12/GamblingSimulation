9ea12166f5098f3d34492569cb982d453922d658
amolnagose12 <amolnagose12@gmail.com>
2021-12-10 01:40:00 +0530

    Risk amount and bet per game

---
+++ src/com/bridgelabz/Gambler.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class Gambler {
+    /*
+     * Creating a gamblerSimulator
+     * Created a method startGambling to store initial capital and risk per game
+     * */
+    public void startGambling() {
+        int startingAmount = 100;
+        int bet_Per_Game = 1;
+    }
+}
