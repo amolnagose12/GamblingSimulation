a239be57673d387d3c258e6d969f900de5dcfec7
amolnagose12 <amolnagose12@gmail.com>
2021-12-10 02:06:15 +0530

    Win or lose by how much

--- src/com/bridgelabz/Gambler.java
+++ src/com/bridgelabz/Gambler.java
@@ -28,20 +28,27 @@ public class Gambler {
 
     public void playFor20Days() {
         int total_Amount_Invested_In_20_Days = 2000;
-        int amount_After_20Days =0;
+        int amount_After_20Days = 0;
+
         for (int i = 1; i <= 20; i++) {
-            amount_After_20Days += startGambling();
+            int result = startGambling();
+            amount_After_20Days += result;
+            if (result == 50) {
+                System.out.println("Lost 50$ in day " + i);
+            }
+            if (result == 150) {
+                System.out.println("Won 50$ in day " + i);
+            }
         }
-        System.out.println("Amount after 20 day is "+ amount_After_20Days);
+        System.out.println("Amount after 20 day is " + amount_After_20Days);
         if (amount_After_20Days > total_Amount_Invested_In_20_Days) {
             int profit = amount_After_20Days - total_Amount_Invested_In_20_Days;
             System.out.println("Total profit after 20 days is = " + profit);
 
-        } else if (amount_After_20Days < total_Amount_Invested_In_20_Days){
+        } else if (amount_After_20Days < total_Amount_Invested_In_20_Days) {
             int loss = total_Amount_Invested_In_20_Days - amount_After_20Days;
             System.out.println("Total loss after 20 days is = " + loss);
-        }
-        else System.out.println("No profit No loss");
+        } else System.out.println("No profit No loss");
     }
 
     public static void main(String[] args) {
